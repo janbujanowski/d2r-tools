153cd54417e87f2fb12e82cd28e74512c87083f7
janbujanowski <jan.bujanowski@gmail.com>
2023-02-07 00:21:41 +0100

    init commit

---
+++ D2runewizardTZRespone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D2Rbots
+{
+    //https://d2runewizard.com/api/terror-zone?token=S8mY7MbhhxMyL7YbmYut6w
+    public class D2runewizardTZRespone
+    {
+        public TerrorZone terrorZone;
+    }
+
+    public class TerrorZone
+    {
+        public string zone;
+        public string act;
+        public HighestProbabilityZone highestProbabilityZone;
+    }
+    public class HighestProbabilityZone
+    {
+        public string zone;
+        public string act;
+        public int amount;
+        public int probability;
+    }
+}
---
+++ DiscordWebhookRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D2Rbots
+{
+    public class DiscordWebhookRequest
+    {
+        public string content;
+        public string username;
+   
+        public DiscordWebhookRequest(D2runewizardTZRespone tzUpdate)
+        {
+            this.username = "D2R TZ bot service";
+            this.content = $"Current TZ : {tzUpdate.terrorZone.zone} with probability : {tzUpdate.terrorZone.highestProbabilityZone.probability}";
+        }
+    }
+}
